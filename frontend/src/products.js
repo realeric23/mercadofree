7ed880c7252d855aacaa3f326f5e9ce7334700d7
realeric23 <ericandresm@yahoo.com>
2021-02-20 02:41:21 -0300

    HomeScreen update

---
+++ frontend/src/products.js
@@ -0,0 +1,22 @@
+const products = [
+  {
+    id: 'MLA785937833',
+    title: 'Zapatillas Marca Rcn Ultraliviana Negra',
+    price: 1769,
+    currency_id: 'ARS',
+    available_quantity: 200,
+    thumbnail: 'http://http2.mlstatic.com/D_728833-MLA32445355209_102019-I.jpg',
+    condition: 'new',
+  },
+  {
+    id: 'MLA716347136',
+    title: 'Zapatillas Ozono Skate Directo De Fábrica',
+    price: 1769,
+    currency_id: 'ARS',
+    available_quantity: 200,
+    thumbnail: 'http://http2.mlstatic.com/D_728833-MLA32445355209_102019-I.jpg',
+    condition: 'new',
+  },
+];
+
+export default products;
