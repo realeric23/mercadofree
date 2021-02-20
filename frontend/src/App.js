a02b7cf2910cf12f19fd1f2976f846f22baaa268
realeric23 <ericandresm@yahoo.com>
2021-02-19 23:11:05 -0300

    front end update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to MercadoFree</h1>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to MercadoFree </h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">
+            Mercado Free <i class="fab fa-typo3"></i>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
