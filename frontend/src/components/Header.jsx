585b196ff3259da7a0aad0ed9f2a77699e797ad8
realeric23 <ericandresm@yahoo.com>
2021-03-05 01:23:43 -0300

    header fix

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,7 +7,9 @@ const Header = () => {
       <header className="masthead text-center text-white">
         <div className="masthead-content ">
           <div className="container " >
-            <h1 className="masthead-heading mb-2 ">Welcome to MercadoFree</h1>
+
+
+            <h1 className="masthead-heading mb-2 mt-5">Welcome to MercadoFree</h1>
             <h2 className="masthead-subheading mb-5">Your place to search</h2>
             <a href="#" className="btn btn-primary btn-xl rounded-pill mt-5 d-none">
               Start Searching NOW
