87ddb290b54286f2d5ff5d60310011fe6057bc25
Eric <ericandresm@yahoo.com>
2021-07-24 01:31:51 -0300

    Version 1.0.3

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const path = require('path');
 
-// const HOST = process.env.HOST || 'http://localhost';
+const HOST = process.env.development.HOST || 'http://localhost';
 const PORT = process.env.PORT || 5000;
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
 
@@ -23,7 +23,7 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(morgan('dev'));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', `${HOST}:3000`); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header(
     'Access-Control-Allow-Headers',
